fca4f044cf16757343ca7845de1006c631956eaa
Alexandre Plateau <lexplt.dev@gmail.com>
2022-09-05 23:23:33 +0200

    Adding the formulae for ArkScript 3.3.0 and 3.2.0

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-# ArkScript-lang Arkscript
+# Homebrew ArkScript
 
 ## How do I install these formulae?
 
 `brew install arkscript-lang/arkscript/<formula>`
 
 Or `brew tap arkscript-lang/arkscript` and then `brew install <formula>`.
+
+You will need to update your `DYLD_LIBRARY_PATH` so that `arkscript` can find its `libArkReactor`:
+```shell
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/opt/homebrew/lib
+```
 
 ## Documentation
 
---
+++ arkscript.rb
@@ -0,0 +1,25 @@
+class Arkscript< Formula
+  desc "Small, fast, functional and scripting language for C++ projects"
+  homepage "https://arkscript-lang.dev"
+  url "https://github.com/ArkScript-lang/Ark.git", tag: "v3.3.0"
+  license "MPL-2.0"
+  head "https://github.com/ArkScript-lang/Ark.git", branch: "dev"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", "-DARK_BUILD_EXE=On", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def post_install
+    ohai "ℹ️  Add ARKSCRIPT_PATH=" + lib + "/Ark/ to your bashrc/zshrc"
+  end
+
+  test do
+    with_env("DYLD_LIBRARY_PATH" => "$DYLD_LIBRARY_PATH:/opt/homebrew/lib") do
+      system "#{bin}/arkscript", "--version"
+    end
+  end
+end
